9d4de3dc0be15a7943c3a5e7417a79d2facd328b
Amrous <625777699@qq.com>
2020-04-30 11:03:53 +0800

    更新accessKeySecret

--- bos_sms/src/main/java/com/baidu/bos/utils/SmsUtils.java
+++ bos_sms/src/main/java/com/baidu/bos/utils/SmsUtils.java
@@ -21,6 +21,9 @@ import com.aliyuncs.profile.IClientProfile;
  * 备注:Demo工程编码采用UTF-8 国际短信发送请勿参照此DEMO
  */
 public class SmsUtils {
+	// amrous_bos_dev
+	public static final String ID = "LTAIagcoAdCepVPR";
+	public static final String SECRET = "BXQBSMIIiwdcoFrSoi463nfJmJu1Ox";
 
 	// 产品名称:云通信短信API产品,开发者无需替换
 	static final String product = "Dysmsapi";
@@ -28,9 +31,9 @@ public class SmsUtils {
 	static final String domain = "dysmsapi.aliyuncs.com";
 
 	// TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
-	static final String accessKeyId = "LTAIagcoAdCepVPR";
-	//BXQBSMIIiwdcoFrSoi463nfJmJu1Ox 三次
-	static final String accessKeySecret = "";
+	static final String accessKeyId = ID;
+
+	static final String accessKeySecret = SECRET;
 
 	public static SendSmsResponse sendSms(String randomCode,String telephone) throws ClientException {
 
